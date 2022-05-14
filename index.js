091345f286d1ae1d9bb22a7960ee0dfed7eddd62
Madeleine Smith <madeleine@madeleinesmith.uk>
2022-05-14 17:26:42 +0100

    Initial BE MVP

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+const updateDevice = require('./updateDevice');
+
+const app = express()
+app.use(express.json());
+
+app.patch("/devices/:id", cors(), async function (req, res) {
+    console.log("hit devices endpoint")
+
+    // todo add this back in after hosting
+    // const deviceId = req.params.id
+    // get device info
+    // const deviceInfo = devices.devicesLookUpObject[deviceId]
+    // const TuyId = deviceInfo["TuyId"]
+    // const TuyKey = deviceInfo["TuyKey"]
+    // const IP = deviceInfo["IP"]
+
+    const brightness = req.body.brightness
+    const colorTemperature = req.body.colorTemperature
+
+    await updateDevice.updateDevice("i", "s", "s", brightness, colorTemperature);
+
+    res.send('Worked!')
+})
+
+const PORT = process.env.PORT || 6789
+app.listen(PORT, () => {
+    console.log(`Mixing it up on port ${PORT}`)
+})
---
+++ updateDevice.js
@@ -0,0 +1,47 @@
+const TuyAPI = require('tuyapi');
+
+async function updateDevice(deviceId, deviceKey, IP, brightness, colorTemperature) {
+    const device = new TuyAPI({
+        id: process.env.DEVICE_ID,
+        key: process.env.DEVICE_KEY,
+    });
+
+// Find device on network
+    device.find()
+        .then(() => {
+            // Connect to device
+            device.connect();
+        })
+        .finally(() => {
+            device.disconnect();
+        });
+
+// Add event listeners
+    device.on('connected', () => {
+        console.log('Connected to device!');
+        device.set({
+            multiple: true,
+            data: {
+                '1': true,
+                '2': 'white',
+                '3': brightness, // brightness
+                '4': colorTemperature // 1 = orange, 255 = blue white
+            }
+        })
+    });
+
+    device.on('disconnected', () => {
+        console.log('Disconnected from device.');
+    });
+
+    device.on('error', error => {
+        console.log('Error!', error);
+    });
+
+// Disconnect after 10 seconds
+    setTimeout(() => {
+        device.disconnect();
+    }, 10000);
+}
+
+module.exports.updateDevice = updateDevice
