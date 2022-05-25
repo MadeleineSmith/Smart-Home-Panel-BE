43148f1a3dd3d65adc702a7bee36168cd31ea2db
Madeleine Smith <madeleine@madeleinesmith.uk>
2022-05-25 22:29:56 +0100

    Pass IP to tuy constructor

--- updateDevice.js
+++ updateDevice.js
@@ -3,14 +3,16 @@ const TuyAPI = require('tuyapi');
 async function updateDevice(deviceId, deviceKey, IP, brightness, colorTemperature) {
     let id = process.env.DEVICE_ID
     let key = process.env.DEVICE_KEY
+    let ip = process.env.DEVICE_IP
 
     const device = new TuyAPI({
         id: id,
         key: key,
         version: 3.3,
+        ip: ip,
     });
 
-    console.log(id, key)
+    console.log(id, key, ip)
 
 // Find device on network
     device.find().then(() => {
